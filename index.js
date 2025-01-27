76461d94e001d575e06c49b76437b278428adf52
Slavo Seman <slavomir.seman@hotovo.com>
2025-01-27 11:51:03 +0100

    Removed

--- index.js
+++ index.js
@@ -6,14 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Sem NEvkladáme žiadny kód pre analýzy či GPT služby.
-// Použijeme route súbory, ktoré importujeme nižšie.
 const topmoversRouter = require('./src/routes/topmovers');
-//const forexRouter = require('./src/routes/forex');
 
 // Priradíme route moduly pod prefix /api
 app.use('/api', topmoversRouter);
-//app.use('/api', forexRouter);
 
 // Spustenie servera
 const PORT = process.env.PORT || 3001;
